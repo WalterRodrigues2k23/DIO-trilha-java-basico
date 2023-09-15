0185b969e331305e7b7c2a732393f897bd3e923b
WalterRodrigues2k23 <metal.walter@gmail.com>
2023-09-15 11:13:20 -0300

    Finalizado exercício Set

--- collections-java-api-2023/src/set/OperacoesBasicas/ConjuntoConvidados.java
+++ collections-java-api-2023/src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class ConjuntoConvidados {
     private Set<Convidado> convidadoSet;
 
-    public ConjuntoConvidados(Set<Convidado> convidadoSet) {
+    public ConjuntoConvidados() {
         this.convidadoSet = new HashSet<>();
     }
 
@@ -32,4 +32,26 @@ public class ConjuntoConvidados {
     public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
+
+    public static void main(String[] args) {
+
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();       
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidados("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidados("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidados("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidados("Convidado 4", 1236);
+
+        System.out.println("");
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        System.out.println("");
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        System.out.println("");
+        conjuntoConvidados.exibirConvidados();
+    }
+
 }
