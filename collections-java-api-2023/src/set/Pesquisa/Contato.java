250e118b3856dfae393595acfd8f847308de12bc
Walter Rodrigues <metal.walter@gmail.com>
2023-09-18 10:16:26 -0300

    Exercícios resolvidos

---
+++ collections-java-api-2023/src/set/Pesquisa/Contato.java
@@ -0,0 +1,21 @@
+package set.Pesquisa;
+
+public class Contato {
+    private String nome;
+    private int numero;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato [nome=" + nome + ", numero=" + numero + "]";
+    }
+
+    
+}
